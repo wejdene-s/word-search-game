57a1eca02c7725ed059595506ad120979149575c
wejdene soussi <wejdene.soussi@gmail.com>
2024-01-23 16:19:36 +0100

    Add userOption.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-
 const initApp = () => {
     displayWords();
 
-    
 
 }
 
---
+++ js/userOption.js
@@ -0,0 +1,18 @@
+import { GenerateRelatedWords } from "./dataFunctions.js";
+
+const initApp = () =>{
+    const start = document.getElementById("start");
+    start.addEventListener("click", getFieldSelected);
+
+}
+
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const getFieldSelected =  async() =>{
+    const select = document.querySelector("select");
+    const  selectedIndex = select.selectedIndex;
+    const selectedOption = select.options[selectedIndex];
+    await GenerateRelatedWords(selectedOption.text);
+
+}
