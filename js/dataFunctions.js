2c28a00ffe6de156dddd0de5bf538ba2d89d8f7b
wejdene soussi <wejdene.soussi@gmail.com>
2024-01-23 15:17:41 +0100

    redirecting the arry of words from index.html to game.html

--- js/dataFunctions.js
+++ js/dataFunctions.js
@@ -1,34 +1,24 @@
-async function GenerateRelatedWords() {
-    const response = await fetch('https://api.datamuse.com/words?ml=science');
-    return response.json();
-}
+export async function GenerateRelatedWords(selectedField) {
+    const response = await fetch(`https://api.datamuse.com/words?ml=${selectedField}`);
+    const words = await response.json();
+    displayWords(words);
+} 
 
-async function displayWords () {
-    const words = await GenerateRelatedWords();
-    let arr ;
-
-    arr = words.filter(element => {
+export function displayWords (words) {
+    let arr = words.filter(element => {
         return element.word.indexOf(" ") === -1 ;
     });
-
-    return arr;
+    randomWords(arr);
 }
 
-const randomWords = async() =>{
-    let arrOfWords = await  displayWords();
-    console.log(arrOfWords);
+export const randomWords = async(arrOfWords) =>{
     let arrOfRandomWords = [];
     while (arrOfRandomWords.length <5){
         let i = Math.floor(Math.random() * arrOfWords.length) ;
-        console.log(i);
         if(arrOfRandomWords.indexOf(arrOfWords[i].word) === -1){
             arrOfRandomWords.push(arrOfWords[i].word);
-        }
-        
+        }  
     }
-    console.log(arrOfRandomWords);
+    localStorage.setItem("arrOfRandomWords", JSON.stringify(arrOfRandomWords));
 
 }
-
-
-randomWords();
---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+console.log(localStorage.getItem("arrOfRandomWords"));
+
+
+
+
