cfedc6a5b940353b1dc9f4bca10c3a0b0f27e9b3
wejdene soussi <wejdene.soussi@gmail.com>
2024-01-24 21:36:59 +0100

    add displayLetters

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const initApp = () => {
-    displayWords();
+    const arrayOfWords = localStorage.getItem("arrOfRandomWords");
+    displayWords(arrayOfWords);
+    displayLetters(arrayOfWords);
+    const letters = document.querySelectorAll(".letter");
+    letters.forEach(letter =>{
+        letter.addEventListener("click", function() {
+            letter.classList.toggle("highlight");
+        })
+    })
+
 
 
 }
@@ -7,24 +16,52 @@ const initApp = () => {
 document.addEventListener("DOMContentLoaded", initApp );
 
 
-const displayWords = () =>{
+const displayWords = (arrayOfWords) =>{
     
     const newDiv = document.createElement("div");
     newDiv.classList.add("words");
     document.getElementById("words-section").appendChild(newDiv);
-    const arrayOfWords = localStorage.getItem("arrOfRandomWords");
     JSON.parse(arrayOfWords).forEach( element =>{
-        console.log(element);
-        createP(newDiv, element);
+        createElem("p", newDiv, element);
     })
 
 }
 
-const createP = (parent, content) => {
-    const newP = document.createElement("p");
-    newP.textContent = content;
-    newP.tabIndex = "0";
-    parent.append(newP);
+const createElem = (type, parent, content) => {
+    const newElem = document.createElement(type);
+    newElem.textContent = content;
+    newElem.tabIndex = "0";
+    if ( type == "div") newElem.setAttribute("class", "letter"); parent.append(newElem);
+
+}
+
+const displayLetters = (arrayOfWords) => {
+    const matrix = document.getElementById("matrix");
+    let totalLength = 0;
+    let arr = [];
+    JSON.parse(arrayOfWords).forEach( element =>{
+        totalLength += element.length;
+        arr.push(element.split(""))
+        for (let c of element){
+            createDiv("div", matrix, c);
+        }
+        
+    })
+    console.log(arr);
+    while (totalLength !== 100){
+        const randomWord = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+        if(arr.indexOf(randomWord) !== -1){
+            createDiv("div", matrix, );
+            totalLength++;
+        }
+    }
+    console.log(totalLength);
+
+    
+}
+
+const createDiv = (type, parent, content) => {
+    createElem(type, parent, content);
 
 }
 
